ce20b8de459ccdd7a808699ce866ff516a0a534f
Rodrigo Gomes <cklrod@hotmail.com>
2023-04-24 17:17:00 -0300

    stateless vs stateful

--- lib/main.dart
+++ lib/main.dart
@@ -2,111 +2,243 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text(
+            'Tarefas',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Colors.yellowAccent,
+        ),
+        body: AnimatedOpacity(
+          opacity: (opacidade == true) ? 1 : 0,
+          duration: const Duration(milliseconds: 800),
+          child: ListView(
+            children: const [
+              Task(
+                'Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                4,
+              ),
+              Task(
+                'Aprender Dart',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                3,
+              ),
+              Task(
+                'Aprender Programação',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                5,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              opacidade = !opacidade;
+            });
+          },
+          backgroundColor: Colors.yellowAccent,
+          child: const Icon(
+            Icons.remove_red_eye,
+            color: Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Task extends StatefulWidget {
+  final String tarefa;
+  final String imagem;
+  final int dificuldade;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const Task(this.tarefa, this.imagem, this.dificuldade, {super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Task> createState() => _TaskState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _TaskState extends State<Task> {
+  int nivel = 0;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: Colors.yellow,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+            height: 140,
+          ),
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white70,
+                ),
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Colors.grey,
+                      ),
+                      width: 82,
+                      height: 100,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(4),
+                        child: Image.network(
+                          widget.imagem,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 200,
+                          child: Text(
+                            widget.tarefa,
+                            style: const TextStyle(
+                                fontSize: 24, overflow: TextOverflow.ellipsis),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.dificuldade >= 1)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.dificuldade >= 2)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.dificuldade >= 3)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.dificuldade >= 4)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.dificuldade >= 5)
+                                  ? Colors.blue
+                                  : Colors.blue[100],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        height: 52,
+                        width: 52,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.yellow,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: const [
+                                Icon(
+                                  Icons.arrow_drop_up,
+                                  color: Colors.black,
+                                ),
+                                Text(
+                                  'UP',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                )
+                              ],
+                            )),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        backgroundColor: Colors.white38,
+                        color: Colors.black,
+                        value: (widget.dificuldade > 0)
+                            ? (nivel / widget.dificuldade) / 10
+                            : 1,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      'Nível: $nivel/10',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,65 @@
+// Container(
+//         color: Colors.white,
+//         child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               Stack(
+//                 alignment: Alignment.center,
+//                 children: [
+//                   Container(
+//                     width: 100,
+//                     height: 100,
+//                     color: Colors.yellow,
+//                   ),
+//                   Container(
+//                     width: 50,
+//                     height: 50,
+//                     color: Colors.orange,
+//                   )
+//                 ],
+//               ),
+//               Stack(
+//                 alignment: Alignment.center,
+//                 children: [
+//                   Container(
+//                     width: 100,
+//                     height: 100,
+//                     color: Colors.yellow,
+//                   ),
+//                   Container(
+//                     width: 50,
+//                     height: 50,
+//                     color: Colors.orange,
+//                   )
+//                 ],
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 children: [
+//                   Container(width: 50, height: 50, color: Colors.greenAccent),
+//                   Container(width: 50, height: 50, color: Colors.grey),
+//                   Container(width: 50, height: 50, color: Colors.pinkAccent),
+//                 ],
+//               ),
+//               Container(
+//                 color: Colors.amberAccent,
+//                 width: 300,
+//                 height: 100,
+//                 child: const Text(
+//                   'Ambâr',
+//                   style: TextStyle(
+//                       color: Colors.black,
+//                       fontSize: 28,
+//                       decoration: TextDecoration.none),
+//                   textAlign: TextAlign.center,
+//                 ),
+//               ),
+//               ElevatedButton(
+//                   onPressed: () {
+//                     print('Ola');
+//                   },
+//                   child: const Text("Aperte")),
+//             ]),
+//       )
